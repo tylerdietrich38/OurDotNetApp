4c247fe4cbc057d6f696a83aa8181150f635924e
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-04-12 16:13:44 -0400

    Done with explorer mode

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace OurDotNetApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#, my name tyler dietrich");
+        }
     }
-  }
 }
