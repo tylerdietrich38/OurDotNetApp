5a18bd49599c38e91769059ec788db20e2f4b828
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-04-13 10:28:24 -0400

    other variables

--- Program.cs
+++ Program.cs
@@ -15,6 +15,18 @@ namespace OurDotNetApp
             var school = "SDG";
 
             var numberofStudents = 11;
+
+            var letter = 'x';
+
+            var letterButDigit = '1';
+
+            var cohort21Started = true;
+
+            var lectureEnded = false;
+
+            var sentence = "The quick brown fox jumped over the lazy dog";
+            var howLong = sentence.Length:
+
         }
     }
 }
