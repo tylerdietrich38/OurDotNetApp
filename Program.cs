9800c451e47fc0408391a8703d234182fbcf48c6
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-04-13 09:49:29 -0400

    using var style

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace OurDotNetApp
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to C#, my name tyler dietrich");
+
+            var name = "Samantha";
+
+            var score = 42;
+
+            var school = "SDG";
+
+            var numberofStudents = 11;
         }
     }
 }
